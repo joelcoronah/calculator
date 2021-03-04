931cceff39b9e7a47e2f7cefb156bfe5b8248f1b
joelcoronah <joelcoronah@gmail.com>
2021-03-04 01:12:25 -0400

    :sparkles: feat: add logic to calculate operations

--- lib/src/pages/calButton_page.dart
+++ lib/src/pages/calButton_page.dart
@@ -2,27 +2,38 @@ import 'package:flutter/material.dart';
 
 class CalButtonPage extends StatelessWidget {
   final String text;
-  const CalButtonPage({Key key, this.text}) : super(key: key);
+  final Color color;
+  final Color backGroundColor;
+  final Function action;
+
+  const CalButtonPage(
+      {Key key, this.text, this.color, this.backGroundColor, this.action})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(5),
-      width: 65,
-      height: 65,
+      width: 70,
+      height: 70,
       child: TextButton(
         style: TextButton.styleFrom(
-          backgroundColor: Colors.white,
+          backgroundColor:
+              backGroundColor != null ? backGroundColor : Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(50)),
           ),
         ),
         onPressed: () {
           print('boton $text');
+          action(text);
         },
         child: Text(
           text,
-          style: TextStyle(fontSize: 40),
+          style: TextStyle(
+            fontSize: 40,
+            color: color != null ? color : Colors.black87,
+          ),
         ),
       ),
     );
--- lib/src/pages/calculator_page.dart
+++ lib/src/pages/calculator_page.dart
@@ -1,8 +1,49 @@
 import 'package:calculator/src/pages/calButton_page.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-class CalculatorApp extends StatelessWidget {
-  const CalculatorApp({Key key}) : super(key: key);
+class CalculatorApp extends StatefulWidget {
+  CalculatorApp({Key key}) : super(key: key);
+
+  @override
+  _CalculatorAppState createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  String _operation = '';
+  String _historical = '';
+
+  void calculate(String numText) {
+    Parser p = Parser();
+    Expression exp = p.parse(_operation);
+
+    // Bind variables:
+    ContextModel cm = ContextModel();
+    // Evaluate expression:
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    setState(() {
+      _historical = _operation;
+      _operation = eval.toString();
+    });
+  }
+
+  void createNum(String text) {
+    setState(() => _operation += text);
+  }
+
+  void clearAll() {
+    setState(() {
+      _operation = '';
+      _historical = '';
+    });
+  }
+
+  void clear() {
+    setState(() {
+      _operation = '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,48 +53,94 @@ class CalculatorApp extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
+            Container(
+              alignment: Alignment(0.8, 1),
+              child: Text(
+                _historical,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment(0.8, 1),
+              child: Text(
+                _operation,
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CalButtonPage(text: 'C'),
-                CalButtonPage(text: 'X'),
-                CalButtonPage(text: '%'),
-                CalButtonPage(text: '/'),
+                CalButtonPage(
+                  text: 'AC',
+                  backGroundColor: Colors.blueAccent,
+                  color: Colors.white,
+                  action: (item) {
+                    clearAll();
+                  },
+                ),
+                CalButtonPage(
+                  text: 'C',
+                  backGroundColor: Colors.blueAccent,
+                  color: Colors.white,
+                  action: createNum,
+                ),
+                CalButtonPage(
+                  text: '%',
+                  color: Colors.blueAccent,
+                  action: createNum,
+                ),
+                CalButtonPage(
+                  text: '/',
+                  color: Colors.blueAccent,
+                  action: createNum,
+                ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CalButtonPage(text: '7'),
-                CalButtonPage(text: '8'),
-                CalButtonPage(text: '9'),
-                CalButtonPage(text: 'X'),
+                CalButtonPage(text: '7', action: createNum),
+                CalButtonPage(text: '8', action: createNum),
+                CalButtonPage(text: '9', action: createNum),
+                CalButtonPage(
+                    text: '*', color: Colors.blueAccent, action: createNum),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CalButtonPage(text: '4'),
-                CalButtonPage(text: '5'),
-                CalButtonPage(text: '6'),
-                CalButtonPage(text: '-'),
+                CalButtonPage(text: '4', action: createNum),
+                CalButtonPage(text: '5', action: createNum),
+                CalButtonPage(text: '6', action: createNum),
+                CalButtonPage(
+                    text: '-', color: Colors.blueAccent, action: createNum),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CalButtonPage(text: '1'),
-                CalButtonPage(text: '2'),
-                CalButtonPage(text: '3'),
-                CalButtonPage(text: '+'),
+                CalButtonPage(text: '1', action: createNum),
+                CalButtonPage(text: '2', action: createNum),
+                CalButtonPage(text: '3', action: createNum),
+                CalButtonPage(
+                    text: '+', color: Colors.blueAccent, action: createNum),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                CalButtonPage(text: '0'),
-                CalButtonPage(text: '.'),
-                CalButtonPage(text: '='),
+                CalButtonPage(text: '.', action: createNum),
+                CalButtonPage(text: '0', action: createNum),
+                CalButtonPage(text: '00', action: createNum),
+                CalButtonPage(
+                    text: '=', color: Colors.blueAccent, action: calculate),
               ],
             ),
           ],
